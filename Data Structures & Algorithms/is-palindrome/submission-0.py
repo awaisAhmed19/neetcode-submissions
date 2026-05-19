845898b591cdbc9450173fec9cbf17a5629b1684
Awais ahmed <awaisahmedoffi@gmail.com>
2026-05-19 15:56:27 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        res=""
+        for ch in s:
+            if ch.isalnum():
+                res+=ch.lower()
+        
+        i=0
+        j=len(res)-1
+        while(i<j):
+            if res[i]!=res[j]:
+                return False
+            i+=1
+            j-=1
+        return True
